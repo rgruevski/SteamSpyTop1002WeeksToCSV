a009f046b96580f969df8795eae34e70d7468351
rgruevski <robgruevski@gmail.com>
2023-05-26 14:22:35 -0400

    Mission Complete

--- Program.cs
+++ Program.cs
@@ -5,33 +5,23 @@ class Program
     static async System.Threading.Tasks.Task Main(string[] args)
     {
         var date = DateTime.Now.ToString("MM-dd");
-        string fileName = $"{date}top1002weeks.csv";
         var url = "http://steamspy.com/api.php?request=top100in2weeks";
-
+        string fileName = $"top1002weeks/{date}.csv";
         try
         {
             using (var httpClient = new HttpClient())
             {
-                // Make the HTTP request to the API
                 var response = await httpClient.GetAsync(url);
                 response.EnsureSuccessStatusCode();
-
-                // Read the response content as string
                 var responseContent = await response.Content.ReadAsStringAsync();
-
-                // Parse the response as JSON and format it into a CSV file
                 var games = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, Game>>(responseContent);
-
                 using (var writer = new StreamWriter(fileName))
                 using (var csv = new CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
                 {
-                    // Write headers
                     Type type = typeof(Game);
                     var gameRefTypeMembers = type.GetProperties().ToList();
                     gameRefTypeMembers.ForEach(m => csv.WriteField(m.Name));
                     csv.NextRecord();
-
-                    // Write game data
                     foreach (var game in games ?? new Dictionary<string, Game>())
                     {
                         csv.WriteField(game.Value.AppId);
